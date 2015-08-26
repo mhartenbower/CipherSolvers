c710d7b7b3b141369e7ad25948b8e8db62ef4af8
Matt Hartenbower <matt.hartenbower@gmail.com>
2015-08-26 13:05:00 -0700

    Added the ability to specify the keyey uused to decrypt

--- CaesarCipherSolver.py
+++ CaesarCipherSolver.py
@@ -1,24 +1,24 @@
 class CaesarCipherSolver:
 
-	def __init__(self, cipherText):
+	def __init__(self, cipherText, key):
 		self.cipherText = cipherText
+		self.key = key
 
 	def decode(self):
-		resultStrings = [0] * 26
-		for i in range(1,25):
-			newString = ""
+		
+		newString = ""
 
-			for j in range(0, len(self.cipherText)):
-				currentChar = ord(self.cipherText[j]) + i
-				if currentChar > 90:
-					currentChar = currentChar - 90 + 64
-				newString = newString + chr(currentChar)
+		for i in range(0, len(self.cipherText)):
+			currentChar = ord(self.cipherText[i]) + self.key
+			if currentChar > 90:
+				currentChar = currentChar - 90 + 64
+			newString = newString + chr(currentChar)
 
-			resultStrings.append(newString)
-		return resultStrings
+		
+		return newString
 
-solve = CaesarCipherSolver("NBYWIOLMYCHZILGUNCIHMBIOFXHIQVYUWWYMMCVFYIHVFUWEVIULX")
+
+solve = CaesarCipherSolver("ZAPQVQEBMDMYMZMZMXACGQBGQPMETMOQDTAK", 10)
 resultList = solve.decode()
-for elem in resultList:
-	print elem
+print resultList
 
