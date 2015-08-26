0e3b3c8d8d08d8bb2252e9d54f1463398dd801c2
Matt Hartenbower <matt.hartenbower@gmail.com>
2015-08-26 13:42:42 -0700

    Fixed an error with calculating the plaintext in decode function

--- CaesarCipherSolver.py
+++ CaesarCipherSolver.py
@@ -4,21 +4,32 @@ class CaesarCipherSolver:
 		self.cipherText = cipherText
 		self.key = key
 
-	def decode(self):
-		
-		newString = ""
+	def encode(self):
+		cipherText = ""
 
-		for i in range(0, len(self.cipherText)):
-			currentChar = ord(self.cipherText[i]) + self.key
+		for i in range(0, len(self.plainText)):
+			currentChar = ord(self.plainText[i]) - self.key
 			if currentChar > 90:
 				currentChar = currentChar - 90 + 64
-			newString = newString + chr(currentChar)
+			cipherText = cipherText + chr(currentChar)
+
+		return cipherText
+
+	def decode(self):
+		
+		plainText = ""
+
+		for i in range(0, len(self.cipherText)):
+			currentChar = ord(self.cipherText[i]) - self.key
+			if currentChar < 65:
+				currentChar = 90 - (64 - currentChar)
+			plainText = plainText + chr(currentChar)
 
 		
-		return newString
+		return plainText
 
 
-solve = CaesarCipherSolver("ZAPQVQEBMDMYMZMZMXACGQBGQPMETMOQDTAK", 10)
+solve = CaesarCipherSolver("MXAEWEHEVOERHWXSVQCRMKLXXLIVEMRJIPPMRXSVVIRXW", 4)
 resultList = solve.decode()
 print resultList
 
